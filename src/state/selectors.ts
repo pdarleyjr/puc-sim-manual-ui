38fcd7f3b72e99f4496942de6fad9f9944ed487a
pdarleyjr <pdarleyjr@users.noreply.github.com>
2025-10-18 15:13:36 -0400

    fix: Remove obsolete selectMasterDischarge selector

--- src/state/selectors.ts
+++ src/state/selectors.ts
@@ -1,11 +1,5 @@
 import type { AppState } from './store'
 
-export const selectMasterDischarge = (st: AppState) =>
-  Math.min(
-    Math.max(...Object.values(st.discharges).map(d => d.open ? d.setPsi : 0), 0),
-    400
-  )
-
 export const selectMasterIntake = (st: AppState) => st.gauges.masterIntake
 
 export const selectRpm = (st: AppState) => st.gauges.rpm
